6e688496ed8461f7d7602abbec755499cff19f36
[s2545171] <aleksanderkirsten@googlemail.com>
2026-01-29 15:02:22 +0100

    creating skeleton for the MCP tools

--- tools/src/aden_tools/tools/parquet_tool/parquet_tool.py
+++ tools/src/aden_tools/tools/parquet_tool/parquet_tool.py
@@ -4,7 +4,30 @@ from ..file_system_toolkits.security import get_secure_path
 
 def register_tools(mcp: FastMCP):
     """Register Parquet Tool with the MCP server."""
+
+    @mcp.tool()
+    def secure_parquet_path(file_path: str) -> str:
+        """ Get the secure path for the parquet file."""
+        return get_secure_path(file_path, allowed_extensions=[".parquet"])
+
     @mcp.tool()
     def parquet_read(file_path: str):
         """ The function to read the the parquet file and return its content as a string or JSON."""
         return {"error": "Not implemented yet."}
+
+    @mcp.tool()
+    def describe_parquet(file_path: str):
+        """ The function to describe the parquet file schema."""
+        return {"error": "Not implemented yet."}
+
+    @mcp.tool()
+    def sample_parquet(file_path: str, n: int = 5):
+        """ The function to return n sample rows from the parquet file."""
+        return {"error": "Not implemented yet."}
+
+    @mcp.tool()
+    def run_sql_on_parquet(file_path: str, query: str):
+        """ The function to run SQL query on the parquet file."""
+        return {"error": "Not implemented yet."}
+
+
